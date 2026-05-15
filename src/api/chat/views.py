a068bc5430fab2cd1a08e6ac6a18cda5c43d7c60
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2026-05-15 11:04:51 +0530

    fix: fixed calling funcition inside Depends to just passing function inside it.

--- src/api/chat/views.py
+++ src/api/chat/views.py
@@ -1,12 +1,17 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlmodel import Session, select
 from api.chat.services import ChatService
 from api.chat.schema import ChatRequest
+from db.session import get_session
 
 
 router = APIRouter();
 chat_service = ChatService();
 
 @router.post("/chat")
-async def chat(prompt: ChatRequest):
+async def chat(
+        prompt: ChatRequest,
+        session: Session = Depends(get_session)
+    ):
     response = await chat_service.chat(prompt.prompt)
     return response
