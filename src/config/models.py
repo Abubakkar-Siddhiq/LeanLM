b18f546412a2d48ea581f0bd4fd3e92076330728
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2026-05-20 20:10:47 +0530

    feat(config): add model constants

---
+++ src/config/models.py
@@ -0,0 +1,11 @@
+# AI Models. Not DB Models!!!
+
+LOW_MODEL = "llama-3.1-8b-instant"
+MEDIUM_MODEL = "qwen/qwen3-32b"
+HIGH_MODEL = "openai/gpt-oss-120b"
+
+MODEL_MAP = {
+    "low": LOW_MODEL,
+    "medium": MEDIUM_MODEL,
+    "high": HIGH_MODEL,
+}
